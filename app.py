63707cf8c4662163b7f54c975bd35ddc8382b7d0
Ali810365 <mabdi@sea.yearup.org>
2025-01-08 20:27:35 -0800

    endpoints spectate, account

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
+import json
+import time
 from lib.client import Client
 from lib.utils import generate_keys
 
+KEY = generate_keys()
+REGION = 'americas'
+client = Client(KEY, REGION)
+
+def get_account():
+    account = client.get_account_by_riot_id("Strompest", "UCI")
+    with open("./output/sample_get_account_response.json", "w") as f:
+        f.write(json.dumps(account, indent=3))
+    
+    return account
+
+def spectate_game():
+    puuid = get_account()['puuid']
+
+    game = client.spectate_game(puuid, 'na1')
+    with open("./output/sample_spectate_game_response.json", "w") as f:
+        f.write(json.dumps(game, indent=3))
+
+    return game
+
+def convert_milliseconds(ms):
+    # Convert milliseconds to seconds
+    seconds = ms // 1000
+    # Calculate minutes and remaining seconds
+    minutes = seconds // 60
+    seconds = seconds % 60
+    # Format as minutes:seconds (e.g., 02:30)
+    return f"{minutes:02}:{seconds:02}"
 
 def main():
-    KEY = generate_keys()
+    #game = spectate_game()
+    with open("./output/sample_spectate_game_response.json", "r") as f:
+        data = json.load(f)
 
-    client = Client(KEY)
+    start_time = data['gameStartTime']
+    current_time = round(time.time() * 1000)
 
-    api_key = client.return_api_key()
+    elapsed_time = current_time - start_time
 
-    print(api_key)
+    elapsed_time_in_minutes = convert_milliseconds(elapsed_time)
+
+    print(elapsed_time_in_minutes)
 
 
 if __name__ == '__main__':
--- lib/client.py
+++ lib/client.py
@@ -1,6 +1,36 @@
+import requests
+
 class Client:
-    def __init__(self, api_key:str):
+    def __init__(self, api_key:str, riot_region:str):
         self.API_KEY = api_key
+        self.RIOT_REGION = riot_region
     
     def return_api_key(self):
         return self.API_KEY
+    
+    def request_headers(self):
+        return {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Riot-Token": self.API_KEY
+        }
+    
+    def get_account_by_riot_id(self, game_name:str, tag_line:str):
+        endpoint = f"https://{self.RIOT_REGION}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+        headers = self.request_headers()
+
+        response = requests.get(endpoint, headers=headers)
+
+        return response.json()
+    
+    def spectate_game(self, puuid:str, in_game_region:str):
+        endpoint = f"https://{in_game_region}.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/{puuid}"
+        headers = self.request_headers()
+
+        response = requests.get(endpoint, headers=headers)
+
+        return response.json()
+
+
+
